756ef897b8a4466b6db16377a0650eeb1b2a9c28
divyakg <divyakg@gmail.com>
2014-10-04 18:32:09 -0400

    Create ApplicationConstants.java

---
+++ REST_CHAT/src/main/java/com/chat/util/ApplicationConstants.java
@@ -0,0 +1,16 @@
+package com.chat.util;
+
+
+public class ApplicationConstants {
+    public static final String CREATE_USR = "/signup";
+    public static final String CREATE_CHAT = "/create";
+    public static final String JOIN_CHAT = "/join";
+    public static final String POST_CHAT = "/post";
+    
+    
+    public static final String STATUS_ACTIVE="ACTIVE";
+    
+    
+    public static final String CHATROOM_AVAIALABLE="AVAIALABLE";
+    public static final String CHATROOM_UNAVAIALABLE="UNAVAIALABLE";
+}
